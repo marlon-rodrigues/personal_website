c72c94011a919ff8b0c70d09a1ef89c332d8bfcf
marlongeraldo <marlon.gerodrigues@gmail.com>
2017-05-02 22:44:29 -0500

    Working on menus

--- javascripts/global.js
+++ javascripts/global.js
@@ -19,23 +19,68 @@ $(document).ready(function(){
 	templateLoad('site-header', null, '#site-header', null);
 		
 		//load main navigation 
-	templateLoad('main-navigation', null, '#main-navigation', null);
+	templateLoad('main-navigation', null, '#main-navigation', setupMainNavigation);
 
 		//load sub navigation 
-	templateLoad('sub-navigation', null, '#sub-navigation', null);
+	templateLoad('sub-navigation', null, '#sub-navigation', setupSubNavigation);
 
-		//load careers
-	templateLoad('careers', null, '#careers', loadCareersSkillsSet);	
+		//load career
+	templateLoad('career', null, '#career', loadcareerSkillsSet);	
+
+		//load education
+	templateLoad('education', null, '#education', loadcareerSkillsSet);	
+
+	function setupMainNavigation() {
+		$('.main-nav-item').click(function(e){
+			if (!$(this).hasClass('active')) {
+				e.preventDefault();
+
+		    	$('.sub-navigation-item').slideUp('slow');
+		    	$('.sub-navigation-item').removeClass('active');
+		    	$('#' + $(this).attr('sub-nav')).slideDown('slow');
+		    	$('#' + $(this).attr('sub-nav')).addClass('active');
+
+		    	$('.main-nav-item').removeClass('active');
+		    	$(this).addClass('active');
+
+		    	var navSection = $(this).children('a').attr('href');
+
+		    	$('html, body').animate({
+				    scrollTop: $(navSection).offset().top - 200
+				}, 750, function(){
+		        	// Add hash (#) to URL when done scrolling (default click behavior)
+		        	window.location.hash = navSection;
+		      	});
+		  	}
+		});
+	}
+
+	function setupSubNavigation() {
+		$('.sub-navigation-item .sub-navigation-body a').click(function(e){
+		  	if($(this).attr("href") !== "") {
+		  		e.preventDefault();
+
+			  	var section = $(this).attr("href");
+
+			  	$('html, body').animate({
+				    scrollTop: $(section).offset().top - 200
+				}, 750, function(){
+		        	// Add hash (#) to URL when done scrolling (default click behavior)
+		        	window.location.hash = section;
+		      	});
+		  	}
+		});
+	}
 
 
-	function createCareersCarousel() {
-		$('.careers-content-wrapper').owlCarousel({
+	function createcareerCarousel() {
+		$('.career-content-wrapper').owlCarousel({
 			items: 1,
 			nav: false
 		});
 	}
 
-	function loadCareersSkillsSet() { 
+	function loadcareerSkillsSet() { 
 		$('.skillbar').each(function() { 
 			$(this).find('.skillbar-bar').animate({
 				width:$(this).attr('data-percent')
