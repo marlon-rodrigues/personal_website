9380e43646edba17cbc5204e63face038c40e3d4
marlongeraldo <marlon.gerodrigues@gmail.com>
2017-05-04 21:02:26 -0500

    Working on skills section

--- javascripts/global.js
+++ javascripts/global.js
@@ -33,6 +33,10 @@ $(document).ready(function(){
 		//load projects
 	templateLoad('projects', null, '#projects', loadProjectsSections);	
 
+		//load skills
+	templateLoad('skills', null, '#skills', buildSkillsGraph);	
+
+
 	function setupMainNavigation() {
 		$('.main-nav-item').click(function(e){
 			if (!$(this).hasClass('active')) {
@@ -78,7 +82,7 @@ $(document).ready(function(){
 	function loadProjectsSections() {
 		templateLoad('projects-ai', null, '#projects-ai', function(){
 			templateLoad('projects-apps', null, '#projects-apps', function(){
-				templateLoad('projects-apps', null, '#projects-apps',loadProjectsCarousels());
+				templateLoad('projects-sites', null, '#projects-sites', loadProjectsCarousels);
 			});
 		});	
 	}
@@ -91,6 +95,18 @@ $(document).ready(function(){
 			loop: true,
 			nav: false
 		});
+
+		$('.projects-carousel-apps').owlCarousel({
+			items: 1,
+			loop: true,
+			nav: false
+		});
+
+		$('.projects-carousel-sites').owlCarousel({
+			items: 1,
+			loop: true,
+			nav: false
+		});
 	}
 
 	function loadcareerSkillsSet() { 
@@ -98,6 +114,66 @@ $(document).ready(function(){
 			$(this).find('.skillbar-bar').animate({
 				width:$(this).attr('data-percent')
 			},6000);
+		});
+	}
+
+	function buildSkillsGraph() {
+		var svg = d3.select("svg"),
+	    width = +svg.attr("width"),
+	    height = +svg.attr("height");
+
+		var format = d3.format(",d");
+
+		var color = d3.scaleOrdinal(d3.schemeCategory20c);
+
+		var pack = d3.pack()
+		    .size([width, height])
+		    .padding(1.5);
+
+		d3.csv("javascripts/skills.csv", function(d) {
+		  d.value = +d.value;
+		  if (d.value) return d;
+		}, function(error, classes) {
+		  if (error) throw error;
+
+		  var root = d3.hierarchy({children: classes})
+		      .sum(function(d) { return d.value; })
+		      .each(function(d) {
+		        if (id = d.data.id) {
+		          var id, i = id.lastIndexOf(".");
+		          d.id = id;
+		          d.package = id.slice(0, i);
+		          d.class = id.slice(i + 1);
+		        }
+		      });
+
+		  var node = svg.selectAll(".node")
+		    .data(pack(root).leaves())
+		    .enter().append("g")
+		      .attr("class", "node")
+		      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+		  node.append("circle")
+		      .attr("id", function(d) { return d.id; })
+		      .attr("r", function(d) { return d.r; });
+		      //.style("fill", function(d) { return color(d.package); });
+
+		  node.append("clipPath")
+		      .attr("id", function(d) { return "clip-" + d.id; })
+		    .append("use")
+		      .attr("xlink:href", function(d) { return "#" + d.id; });
+
+		  node.append("text")
+		      .attr("clip-path", function(d) { return "url(#clip-" + d.id + ")"; })
+		    .selectAll("tspan")
+		    .data(function(d) { return d.class.split(/(?=[A-Z][^A-Z])/g); })
+		    .enter().append("tspan")
+		      .attr("x", 0)
+		      .attr("y", function(d, i, nodes) { return 13 + (i - nodes.length / 2 - 0.5) * 10; })
+		      .text(function(d) { return d; });
+
+		  node.append("title")
+		      .text(function(d) { return d.id + "\n" + format(d.value); });
 		});
 	}
 });
